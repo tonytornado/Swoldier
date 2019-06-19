c194ffb54ee9dbe2fa8ef6ed34c0c68ba79d28d5
Tony T <TonyTatsumaki@live.com>
2019-06-19 12:59:07 -0400

    Change to an API key

---
+++ OCFX_Library/Data/DataModels/FitnessModels/CalorieBurn.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OCFX_Library.Data.DataModels.FitnessModels
+{
+    public class CalorieBurn
+    {
+        
+    }
+}
