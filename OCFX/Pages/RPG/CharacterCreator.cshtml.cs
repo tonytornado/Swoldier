ea98bfa9a5a07220c55bc491402a76ef0200420e
Tony T <TonyTatsumaki@live.com>
2019-08-20 12:08:46 -0400

    Character creation tool added.
    Now, about editing characters after the fact...

--- OCFX/Pages/RPG/CharacterCreator.cshtml.cs
+++ OCFX/Pages/RPG/CharacterCreator.cshtml.cs
@@ -1,9 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OCFX.Areas.Identity.Data;
 using OCFX.DataModels;
 
@@ -12,35 +17,59 @@ namespace OCFX.Pages.RPG
     public class CharacterCreatorModel : PageModel
     {
         private readonly OCFXContext _context;
+        private readonly UserManager<OCFXUser> _userManager;
+        private readonly IHostingEnvironment _environment;
+        private string fileName;
+
+        public CharacterCreatorModel(OCFXContext context, UserManager<OCFXUser> userManager, IHostingEnvironment environment)
+        {
+            _context = context ?? throw new ArgumentNullException(nameof(context));
+            _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+            _environment = environment ?? throw new ArgumentNullException(nameof(environment));
+        }
 
         public List<Archetype> Classes { get; set; }
         public List<Skill> Skills { get; set; }
+        public SelectList ClassList { get; set; }
         [BindProperty]
         public CreatorModel Input { get; set; }
+        public string StatusMessage { get; private set; }
 
         public class CreatorModel
         {
-            public string FirstName { get; internal set; }
-            public string LastName { get; internal set; }
-            public int STR { get; internal set; }
-            public int DEX { get; internal set; }
-            public int CON { get; internal set; }
-            public int VIT { get; internal set; }
-            public int SPD { get; internal set; }
-            public int MVN { get; internal set; }
-            public string Backstory { get; internal set; }
-            public string Drive { get; internal set; }
-            public string Goal { get; internal set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public int STR { get; set; }
+            public int DEX { get; set; }
+            public int CON { get; set; }
+            public int VIT { get; set; }
+            public int SPD { get; set; }
+            public int MVN { get; set; }
+            public string Backstory { get; set; }
+            public string Drive { get; set; }
+            public string Goal { get; set; }
+            public int Class { get; set; }
+
+            public IFormFile Avatar { get; set; }
         }
 
         public void OnGet()
         {
             Classes = _context.Archetypes.ToList();
             Skills = _context.Skills.ToList();
+
+            ClassList = new SelectList(Classes, "Id", "FitType", null);
         }
 
-        public IActionResult OnPost()
+        public async Task<IActionResult> OnPost()
         {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+
             var Character = new CharacterModel()
             {
                 FirstName = Input.FirstName,
@@ -54,11 +83,58 @@ namespace OCFX.Pages.RPG
                 BackStory = Input.Backstory,
                 DriveStory = Input.Drive,
                 Goals = Input.Goal,
+                FitStyle = _context.Archetypes.FirstOrDefault(c => c.Id == Input.Class),
+                CharacterProfile = _context.Profiles.FirstOrDefault(c => c.Id == user.ProfileId)
             };
+
+            Photo avatar = new Photo()
+            {
+                Type = Photo.PhotoType.Avatar,
+                Caption = "My avatar photo",
+                DateAdded = DateTime.Now,
+                ProfileId = Character.CharacterProfile.Id
+            };
+            
+
+            // Add the photo for the avatar
+            fileName = GetUniqueName(Input.Avatar.FileName);
+            string folderPath = $"images/{avatar.ProfileId}/profilePhoto";
+            string upload = Path.Combine(_environment.WebRootPath, folderPath);
+            CheckFolderPath(upload);
+            string filePath = Path.Combine(upload, fileName);
+            await Input.Avatar.CopyToAsync(new FileStream(filePath, FileMode.Create));
+            avatar.URL = $"../images/{avatar.ProfileId}/avatarPhoto/{fileName}";
+
+            StatusMessage = "Your avatar has been created!";
+
+            _context.Photos.Add(avatar);
             _context.Characters.Add(Character);
             _context.SaveChanges();
 
             return RedirectToPage("./Dashboard/");
+        }
+
+        /// <summary>
+        /// Checks for folder on the server; and creates it if necessary
+        /// </summary>
+        /// <param name="v">The folder path</param>
+        private void CheckFolderPath(string v)
+        {
+            if (!Directory.Exists(v))
+            {
+                Directory.CreateDirectory(v);
+            }
+        }
+
+        /// <summary>
+        /// Create a unique file name for the file being uploaded.
+        /// </summary>
+        /// <param name="fileName">A filename string</param>
+        /// <returns></returns>
+        private string GetUniqueName(string fileName)
+        {
+            fileName = Path.GetFileName(fileName);
+            return $"{Path.GetFileNameWithoutExtension(fileName)}_{Guid.NewGuid().ToString().Substring(0, 6)}{Path.GetExtension(fileName)}";
         }
     }
 }
